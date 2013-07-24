a046af9a293a92ad96ef67e897435d37a5e6c345
Dan Peterson <dpiddy@gmail.com>
2013-07-24 11:04:41 -0300

    Collapse log lines.

--- forwarder.go
+++ forwarder.go
@@ -63,8 +63,7 @@ func (f *Forwarder) connect() {
 			Logf("measure.log-iss.forwarder.connect.error=1 id=%d message=%q", f.Id, err)
 			f.disconnect()
 		} else {
-			Logf("measure.log-iss.forwarder.connect.duration=%dms id=%d", time.Since(start)/time.Millisecond, f.Id)
-			Logf("measure.log-iss.forwarder.connect.success=1 id=%d", f.Id)
+			Logf("measure.log-iss.forwarder.connect.success=1 measure.log-iss.forwarder.connect.duration=%dms id=%d", time.Since(start)/time.Millisecond, f.Id)
 			f.c = c
 			return
 		}
@@ -89,8 +88,7 @@ func (f *Forwarder) write(p *Payload) {
 			Logf("measure.log-iss.forwarder.write.error=1 id=%d request_id=%q message=%q", f.Id, p.RequestId, err)
 			f.disconnect()
 		} else {
-			Logf("measure.log-iss.forwarder.write.success.messages=1 id=%d request_id=%q remote_addr=%s", f.Id, p.RequestId, f.c.RemoteAddr().String())
-			Logf("measure.log-iss.forwarder.write.success.bytes=%d id=%d request_id=%q remote_addr=%s", n, f.Id, p.RequestId, f.c.RemoteAddr().String())
+			Logf("measure.log-iss.forwarder.write.success.messages=1 measure.log-iss.forwarder.write.success.bytes=%d id=%d request_id=%q remote_addr=%s", n, f.Id, p.RequestId, f.c.RemoteAddr().String())
 			return
 		}
 	}
