7e08d50874e6e1ea6190c8ece89229a99a7a874c
Edward Muller <edwardam@interlix.com>
2015-02-06 19:43:13 -0800

    test

--- cmd/admin/main.go
+++ cmd/admin/main.go
@@ -9,11 +9,17 @@ import (
 	"github.com/heroku/log-iss/Godeps/_workspace/src/github.com/kr/secureheader"
 )
 
-var templates = template.Must(template.ParseFiles("cmd/admin/view.html"))
+var index = template.Must(template.ParseFiles("cmd/admin/ui/_base.tmpl", "cmd/admin/ui/index.tmpl"))
+var there = template.Must(template.ParseFiles("cmd/admin/ui/_base.tmpl", "cmd/admin/ui/there.tmpl"))
 
 func listData(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "view.html", nil)
-	if err != nil {
+	if err := index.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func thereHandler(w http.ResponseWriter, r *http.Request) {
+	if err := there.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
@@ -23,6 +29,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", listData)
+	mux.HandleFunc("/there/", thereHandler)
 
 	behindGoogleAuth := &googlegoauth.Handler{
 		RequireDomain: os.Getenv("REQUIRE_DOMAIN"),
