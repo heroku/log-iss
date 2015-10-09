c64d1e75ed34aa3898cc2d34745820cd4ac6cd0d
Cyril David <cyril@heroku.com>
2015-10-09 11:57:30 -0700

    Make lint/vet happy

--- cmd/forwarder/fixer.go
+++ cmd/forwarder/fixer.go
@@ -12,11 +12,7 @@ import (
 const (
 	// LogplexDefaultHost is the default host from logplex:
 	// https://github.com/heroku/logplex/blob/master/src/logplex_http_drain.erl#L443
-<<<<<<< HEAD
 	logplexDefaultHost = "host"
-=======
-	LogplexDefaultHost = "host"
->>>>>>> 0535ff4... Make lint/vet happy
 )
 
 var nilVal = []byte(`- `)
@@ -35,11 +31,7 @@ func fix(r io.Reader, remoteAddr string, logplexDrainToken string) ([]byte, erro
 		messageWriter.WriteString(" ")
 		messageWriter.Write(header.Time)
 		messageWriter.WriteString(" ")
-<<<<<<< HEAD
 		if string(header.Hostname) == logplexDefaultHost && logplexDrainToken != "" {
-=======
-		if string(header.Hostname) == LogplexDefaultHost && logplexDrainToken != "" {
->>>>>>> 0535ff4... Make lint/vet happy
 			messageWriter.WriteString(logplexDrainToken)
 		} else {
 			messageWriter.Write(header.Hostname)
