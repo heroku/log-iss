619d8f5df2933c46b51a10b98bd22ad28e2dea42
Michael Shea <michael.shea@heroku.com>
2019-02-04 12:44:15 +1100

    Modify code so that we use the `hmac` field from json, rather than the "value" field, to make it clear that we are dealing with *hashes* rather than *passwords*.

--- cmd/forwarder/auth.go
+++ cmd/forwarder/auth.go
@@ -19,11 +19,6 @@ import (
 	metrics "github.com/rcrowley/go-metrics"
 )
 
-type Credential struct {
-	Stage string `json:stage`
-	Value string `json:value`
-}
-
 func newAuth(config AuthConfig, registry metrics.Registry) (*BasicAuth, error) {
 	var err error
 
@@ -163,7 +158,8 @@ func NewBasicAuthFromString(creds string, hmacKey string, registry metrics.Regis
 			return ba, fmt.Errorf("Unable to create credentials from '%s'", u)
 		}
 
-		ba.AddPrincipal(uparts[0], hmacEncode(hmacKey, uparts[1]), "env")
+		value := uparts[1]
+		ba.AddPrincipal(uparts[0], hmacEncode(hmacKey, value), "env")
 	}
 	return ba, nil
 }
@@ -185,13 +181,13 @@ func NewBasicAuth(registry metrics.Registry, hmacKey string) *BasicAuth {
 }
 
 // AddPrincipal add's a user/password combo to the list of valid combinations
-func (ba *BasicAuth) AddPrincipal(user string, password string, stage string) {
+func (ba *BasicAuth) AddPrincipal(user string, hmac string, stage string) {
 	ba.Lock()
 	u, existed := ba.creds[user]
 	if !existed {
 		u = make([]Credential, 0, 1)
 	}
-	ba.creds[user] = append(u, Credential{Stage: stage, Value: password})
+	ba.creds[user] = append(u, Credential{Stage: stage, Hmac: hmac})
 	ba.Unlock()
 }
 
@@ -207,9 +203,9 @@ func (ba *BasicAuth) Authenticate(r *http.Request) bool {
 	defer ba.RUnlock()
 
 	if credentials, exists := ba.creds[user]; exists {
-		for _, credential := range credentials {
-			if credential.Value == hmacEncode(ba.hmacKey, pass) {
-				countName := fmt.Sprintf("log-iss.auth.successes.%s.%s", user, credential.Stage)
+		for _, c := range credentials {
+			if c.Hmac == hmacEncode(ba.hmacKey, pass) {
+				countName := fmt.Sprintf("log-iss.auth.successes.%s.%s", user, c.Stage)
 				counter := metrics.GetOrRegisterCounter(countName, ba.registry)
 				counter.Inc(1)
 				return true
